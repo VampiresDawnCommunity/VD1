c2533ac0ef02af80892926cd63e6aef8f7e0d779
florianessl <github@iessl.at>
2025-07-05 03:13:58 +0200

    Translation: Updated script to trim out all strings with context "actors.name" for map files

--- Language/en/update.ps1
+++ Language/en/update.ps1
@@ -3,5 +3,15 @@
 ./lcftrans.exe -u ../../
 
 Get-ChildItem ./* -Include ('*.po') | Foreach-Object {
+	if ($_.Name.startswith("Map")) {
+		$content = Get-Content -Path $_.Name -Encoding UTF8 -Raw |% {$_-replace 'msgctxt "actors.name"\r\nmsgid "[^"]*"\r\nmsgstr ""\r\n\r\n', '###'}
+		$content = $content.trim()
+		Set-Content -Path $_.Name -Encoding UTF8 -Value $content
+	}
 	Set-Content -Path $_.Name -Encoding UTF8 -Value (Get-Content -Path $_.Name -Encoding UTF8 | Select-String -Pattern '^#.*' -NotMatch)
+	
+	$count = (get-content -Path $_.Name | select-string -pattern "msgstr").length
+	if ($count -eq 1) {
+		rm $_.Name
+	}
 }
